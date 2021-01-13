3a37ae655b503b7b3327eee3f851a1622fd76efe
fahadsaleem1 <igz.iwd.25@gmail.com>
2021-01-13 12:05:58 +0500

    add

--- pages/api/contact/test.js
+++ pages/api/contact/test.js
@@ -1,5 +1,29 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+const shortid = require('shortid');
+
 export default function handler(req, res) {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ name: 'John Doe' }))
+    
+
+    if (req.method === 'POST') {
+      var name = req.body.name
+      var last_name = req.body.last_name
+      var email = req.body.email
+      var message = req.body.message
+
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify({ name: name }))
   }
+
+  
+
+  
+
+
+
+
+
+}
